a02e0dd81857a557ac6f232632cf8486fc9c73c1
Mayank <mayanktiwari1212.mt@gmail.com>
2024-10-18 20:20:26 +0530

    feat: Database setup

--- src/services/databaseService.ts
+++ src/services/databaseService.ts
@@ -1,15 +1,27 @@
-import mongoose from "mongoose";
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PrismaClient } from "@prisma/client";
 import { AppConfig } from "../config";
 
-export default {
-  connect: async () => {
-    try {
-      // Connect to MongoDB
-      await mongoose.connect(AppConfig.get("DATABASE_URL") as string);
-      return mongoose.connection;
-    } catch (error) {
-      throw error;
-    }
+let prisma: PrismaClient;
+
+declare global {
+  // eslint-disable-next-line no-var, @typescript-eslint/no-redundant-type-constituents
+  var __prisma: PrismaClient | undefined;
+}
+
+if (AppConfig.get("ENV") === "production") {
+  prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"]
+  });
+} else {
+  if (!global.__prisma) {
+    global.__prisma = new PrismaClient({
+      log: ["query", "info", "error", "warn"]
+    });
   }
-};
+  prisma = global.__prisma;
+}
+
+export default prisma;
 
