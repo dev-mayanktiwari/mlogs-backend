34c629f898352d59c17688af78209c59c6856eb3
Mayank <mayanktiwari1212.mt@gmail.com>
2024-11-22 12:55:32 +0530

    feat: Fetch blog API

--- src/router/blogRouter.ts
+++ src/router/blogRouter.ts
@@ -3,7 +3,6 @@ import blogController from "../controller/blogController";
 
 const blogRouter = Router();
 
-blogRouter.get("/fetch-blogs", blogController.fetchBlogs);
 blogRouter.post("/:blogId/like", blogController.like);
 blogRouter.delete("/:blogId/unlike", blogController.unlike);
 blogRouter.get("/:blogId/total-likes", blogController.totalLikes);
@@ -15,7 +14,6 @@ blogRouter.put("/:commentId/edit-comment", blogController.editComment);
 blogRouter.post("/:blogId/save", blogController.save);
 blogRouter.delete("/:blogId/unsave", blogController.unsave);
 blogRouter.get("/:blogId/total-saves", blogController.totalSaves);
-blogRouter.get("/:blogId/blog", blogController.getBlog);
 
 // Liking a blog
 // Commenting on a blog
