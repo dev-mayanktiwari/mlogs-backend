81985efeff2091023fec5148dc8575fa92bcb66a
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-04 17:58:01 +0530

    feat: Updated schema

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -5,17 +5,20 @@ import { MAX_LENGTH_MESSAGE, MIN_LENGTH_MESSAGE } from "../constant/responseMess
 export const registerUserSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(EUserTypeConstants.MIN_NAME_LENGTH, MIN_LENGTH_MESSAGE("Name", EUserTypeConstants.MIN_NAME_LENGTH))
     .max(EUserTypeConstants.MAX_NAME_LENGTH, MAX_LENGTH_MESSAGE("Name", EUserTypeConstants.MAX_NAME_LENGTH))
     .regex(/^[A-Za-z\s]+$/, "Name can only contain alphabets and spaces"),
-  email: z.string().email(),
+  email: z.string().trim().email(),
   username: z
     .string()
+    .trim()
     .min(EUserTypeConstants.MIN_USERNAME_LENGTH, MIN_LENGTH_MESSAGE("Username", EUserTypeConstants.MIN_USERNAME_LENGTH))
     .max(EUserTypeConstants.MAX_USERNAME_LENGTH, MAX_LENGTH_MESSAGE("Username", EUserTypeConstants.MAX_USERNAME_LENGTH))
     .regex(/^[A-Za-z0-9_]+$/, "Username can only contain letters, numbers, and underscores"),
   password: z
     .string()
+    .trim()
     .min(EUserTypeConstants.MIN_PASSWORD_LENGTH, MIN_LENGTH_MESSAGE("Password", EUserTypeConstants.MIN_PASSWORD_LENGTH))
     .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH))
     .regex(/(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
@@ -23,20 +26,22 @@ export const registerUserSchema = z.object({
 });
 
 export const loginUserSchema = z.object({
-  email: z.string(),
+  email: z.string().trim(),
   password: z
     .string()
+    .trim()
     .min(EUserTypeConstants.MIN_PASSWORD_LENGTH, MIN_LENGTH_MESSAGE("Password", EUserTypeConstants.MIN_PASSWORD_LENGTH))
     .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH))
 });
 
 export const forgotPasswordSchema = z.object({
-  email: z.string().email()
+  email: z.string().trim().email()
 });
 
 export const resetPasswordSchema = z.object({
   newPassword: z
     .string()
+    .trim()
     .min(EUserTypeConstants.MIN_PASSWORD_LENGTH, MIN_LENGTH_MESSAGE("Password", EUserTypeConstants.MIN_PASSWORD_LENGTH))
     .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH))
     .regex(/(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
@@ -47,17 +52,18 @@ export const changePasswordSchema = z
   .object({
     oldPassword: z
       .string()
+      .trim()
       .min(EUserTypeConstants.MIN_PASSWORD_LENGTH, MIN_LENGTH_MESSAGE("Password", EUserTypeConstants.MIN_PASSWORD_LENGTH))
-      .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH))
-      .regex(/(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
-      .regex(/(?=.*[!@#$%^&*(),.?":{}|<>])/, "Password must contain at least one special character"),
+      .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH)),
+
     newPassword: z
       .string()
+      .trim()
       .min(EUserTypeConstants.MIN_PASSWORD_LENGTH, MIN_LENGTH_MESSAGE("Password", EUserTypeConstants.MIN_PASSWORD_LENGTH))
       .max(EUserTypeConstants.MAX_PASSWORD_LENGTH, MAX_LENGTH_MESSAGE("Password", EUserTypeConstants.MAX_PASSWORD_LENGTH))
       .regex(/(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
       .regex(/(?=.*[!@#$%^&*(),.?":{}|<>])/, "Password must contain at least one special character"),
-    confirmNewPassword: z.string()
+    confirmNewPassword: z.string().trim()
   })
   .refine((data) => data.newPassword === data.confirmNewPassword, {
     path: ["confirmNewPassword"],
