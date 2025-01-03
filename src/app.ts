45ca0bef1012b9c35ffe6513ac68bb605aa102f9
Mayank <mayanktiwari1212.mt@gmail.com>
2025-01-03 16:22:43 +0530

    fix: CORS issue

--- src/app.ts
+++ src/app.ts
@@ -15,15 +15,20 @@ import adminAuthRouter from "./router/adminAuthRouter";
 import adminAuthMiddleware from "./middleware/adminAuthentication";
 import adminBlogRouter from "./router/adminBlogRouter";
 import fetchBlogRouter from "./router/fetchBlogRouter";
-import corsOptions from "./services/corsService";
+import { AppConfig } from "./config";
 
 const app: Application = express();
-const allowedOrigins = corsOptions();
+// const allowedOrigins = corsOptions();
 
 // Middleware
 app.use(helmet());
 app.use(cookieParser());
-app.use(cors(allowedOrigins));
+app.use(
+  cors({
+    origin: AppConfig.get("CORS_ORIGIN") as string,
+    credentials: true
+  })
+);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
